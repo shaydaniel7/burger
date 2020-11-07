280e3b3cd5774b3627854e2df105796bdc10263d
shay_daniel <shay@isom.org>
2020-11-06 21:55:51 -0800

    jaws

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,26 @@
 // Dependencies
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Peewee77!",
-  database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "root",
+		password: "Peewee77!",
+		database: "burger_db"
+	});
+}
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Peewee77!",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
